3fc72fe8fff911359f9b74861f5f5cebc1fd9fbc
jinhui <seahee422@gmail.com>
2025-11-24 16:33:49 +0900

    Header, Main, Footer 생성

---
+++ src/components/section/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/**
+ * 
+ * role이란?
+ *  웹표준을 지켜주기 위해서 이 사이트의 역할을 나타나게 해줌
+ */
+const Header = () => {
+  return (
+    <header id='header' role='banner'>
+        Header
+    </header>
+  )
+}
+
+export default Header
